bf5160f39ae37196851f440c9dc8e50ed60179ea
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2022-10-06 21:46:14 +0700

    Add service activity

---
+++ src/services/activity.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+class ActivityService {
+  http = axios.create({
+    baseURL: process.env.REACT_APP_API,
+  });
+
+  async getActivity() {
+    const email = process.env.REACT_APP_EMAIL;
+    const response = await this.http.get(`/activity-groups?email=${email}`);
+    return response.data.data;
+  }
+
+  async getDetailActivity(id) {
+    const response = await this.http.get(`activity-groups/${id}`);
+    return response.data.data;
+  }
+
+  async createActivity(comment, email, title) {
+    const response = await this.http.post("/activity-groups", {
+      title,
+      email,
+      comment,
+    });
+    return response.data;
+  }
+
+  async deleteActivity(id) {
+    const response = await this.http.delete(`/activity-groups/${id}`);
+    return response.data;
+  }
+}
+
+export default new ActivityService();
