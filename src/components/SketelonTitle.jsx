54f12b0ef25a041308841f46f87af12eb5f77880
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2022-10-07 20:17:08 +0700

    Add sketelon loading for title

---
+++ src/components/SketelonTitle.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SketelonTitle = () => {
+  return (
+    <p className="h-[30px] w-[200px] animate-pulse rounded-full bg-slate-700/20"></p>
+  );
+};
+
+export default SketelonTitle;
